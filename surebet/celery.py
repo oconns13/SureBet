12438c014cc588a762ed75fe90fea9aac3f4f934
Jared O'Connor <jrod13rocks@gmail.com>
2025-01-20 18:39:06 -0500

    remove key

--- surebet/celery.py
+++ surebet/celery.py
@@ -5,5 +5,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surebet.settings')
 
 app = Celery('surebet')
 app.config_from_object('django.conf:settings')
-app.conf.broker_url = 'sqs://AKIATBRPQBCHTIQNLFV2:twcI0JvTAXnW8213eKjq0pv+S8PzmrU8BJfdC09H@'
 app.autodiscover_tasks()
